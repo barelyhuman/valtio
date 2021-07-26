5046ab78107077a880bdcb92ae7cb4c32d46689a
Siddharth <ahoy@barelyhuman.dev>
2021-07-26 10:18:27 +0530

    the graceful version

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-export * from "./vanilla";
-export * from "./react";
-export * as default from "./vanilla";
-
-
-
-
+export { useSnapshot } from "./react"
+export {
+  ref,
+  proxy,
+  getVersion,
+  subscribe,
+  snapshot,
+} from "./vanilla"
